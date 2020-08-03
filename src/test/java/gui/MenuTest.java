4aa8b056cccb02b90aabed0daf6d6a82fe707433
nima7920 <alizaden.1379@gmail.com>
2020-08-03 12:29:09 +0430

    MenuTest class added in test part GameData added from previous part ( will be edited)

---
+++ src/test/java/gui/MenuTest.java
@@ -0,0 +1,28 @@
+package gui;
+
+import client.gui.GameMenu;
+import client.gui.login.LoginMenu;
+
+import javax.swing.*;
+
+public class MenuTest extends JFrame {
+
+    public MenuTest(GameMenu gameMenu){
+        setLayout(null);
+        setResizable(false);
+//        initMenus();
+//        //gameFrame.add(mainMenu);
+        setSize(gameMenu.getSize());
+        getContentPane().add(gameMenu);
+        setVisible(true);
+        setLocationRelativeTo(null);
+//
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new MenuTest(new LoginMenu());
+    }
+}
