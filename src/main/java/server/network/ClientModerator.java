bd13ca19c8d8719a30ef9b9cca2802e0019940e1
nima7920 <alizaden.1379@gmail.com>
2020-08-04 04:07:29 +0430

    ClientModerator added

---
+++ src/main/java/server/network/ClientModerator.java
@@ -0,0 +1,12 @@
+package server.network;
+
+import java.net.Socket;
+
+public class ClientModerator extends Thread{
+
+    private Socket socket;
+    private Server server;
+    private String authToken;
+
+
+}
