e554433d5be33ba6714d84a447361aad169fdae0
nima7920 <alizaden.1379@gmail.com>
2020-08-04 11:38:20 +0430

    basics of ClientModerator added

--- src/main/java/server/network/ClientModerator.java
+++ src/main/java/server/network/ClientModerator.java
@@ -1,12 +1,39 @@
 package server.network;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class ClientModerator extends Thread{
+public class ClientModerator extends Thread {
 
     private Socket socket;
     private Server server;
     private String authToken;
+    private Scanner in;
+    private PrintWriter out;
+
+    public ClientModerator(Server server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+        initHandlers();
+
+    }
+
+    private void initHandlers() {
+        try {
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream());
+
+        }catch(IOException e){
+
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
 
 
 }
