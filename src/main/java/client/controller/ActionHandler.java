fbc384507ebf8bb328884f3d8917430491c9884a
nima7920 <alizaden.1379@gmail.com>
2020-08-06 12:15:17 +0430

    Method signatures added to ActionHandler store Request Types executed in Receiver

--- src/main/java/client/controller/ActionHandler.java
+++ src/main/java/client/controller/ActionHandler.java
@@ -3,6 +3,7 @@ package client.controller;
 import client.gui.GameScreen;
 import client.gui.login.LoginActions;
 import client.gui.main.MainActions;
+import client.gui.store.StoreActions;
 import client_server_interfaces.Response;
 
 public class ActionHandler {
@@ -11,6 +12,7 @@ public class ActionHandler {
     // actions classes
     private LoginActions loginActions;
     private MainActions mainActions;
+    private StoreActions storeActions;
 
     public ActionHandler() {
         gameScreen = GameScreen.getInstance();
@@ -20,15 +22,44 @@ public class ActionHandler {
     private void initActions() {
         loginActions = (LoginActions) gameScreen.getMenuActions("login");
         mainActions = (MainActions) gameScreen.getMenuActions("main");
-
+        storeActions = (StoreActions) gameScreen.getMenuActions("store");
     }
+
     // login methods
-    public void loginAction(Response response){
-        if(response.getResultCode()==1 && !response.getRequestType().toString().equals("login_delete")){
+    public void loginAction(Response response) {
+        if (response.getResultCode() == 1 && !response.getRequestType().toString().equals("login_delete")) {
             gameScreen.gotoMenu("main");
-        }else{
+        } else {
             loginActions.showError(response.getResultCode());
         }
+
+    }
+
+    public void backToMain(){
+
+
+    }
+
+    public void exitGame(){
+
+
+    }
+    public void store_showBuyableCards(Response response){
+
+
+    }
+    public void store_showSalableCards(Response response){
+
+
+    }
+    public void store_selectCard(Response response){
+
+
+    }
+    public void store_buyCard(Response response){
+
+    }
+    public void store_sellCard(Response response){
 
     }
 }
--- src/main/java/client/controller/Receiver.java
+++ src/main/java/client/controller/Receiver.java
@@ -9,15 +9,15 @@ public class Receiver {
     private static ActionHandler actionHandler;
     private GsonHandler gsonHandler;
 
-    public Receiver(){
-        actionHandler=new ActionHandler();
-        gsonHandler=new GsonHandler();
+    public Receiver() {
+        actionHandler = new ActionHandler();
+        gsonHandler = new GsonHandler();
     }
 
-    public void handleResponse(String responseString){
-        Response response=gsonHandler.deserializeResponse(responseString);
-        if(response!=null){
-            RequestTypeClient requestTypeClient=RequestTypeClient.valueOf(response.getRequestType().toString());
+    public void handleResponse(String responseString) {
+        Response response = gsonHandler.deserializeResponse(responseString);
+        if (response != null) {
+            RequestTypeClient requestTypeClient = RequestTypeClient.valueOf(response.getRequestType().toString());
             requestTypeClient.execute(response);
         }
     }
@@ -26,7 +26,7 @@ public class Receiver {
         login_login {
             @Override
             public void execute(Response response) {
-actionHandler.loginAction(response);
+                actionHandler.loginAction(response);
             }
         }, login_signUp {
             @Override
@@ -38,7 +38,42 @@ actionHandler.loginAction(response);
             public void execute(Response response) {
                 actionHandler.loginAction(response);
             }
+        }, backToMain {
+            @Override
+            public void execute(Response response) {
+                actionHandler.backToMain();
+            }
+        }, exitGame {
+            @Override
+            public void execute(Response response) {
+                actionHandler.exitGame();
+            }
+        },
+        store_buyPanel {
+            @Override
+            public void execute(Response response) {
+                actionHandler.store_showBuyableCards(response);
+            }
+        }, store_sellPanel {
+            @Override
+            public void execute(Response response) {
+                actionHandler.store_showSalableCards(response);
+            }
+        }, store_cardSelect {
+            @Override
+            public void execute(Response response) {
+                actionHandler.store_selectCard(response);
+            }
+        }, store_buyCard {
+            @Override
+            public void execute(Response response) {
+                actionHandler.store_buyCard(response);
+            }
+        }, store_sellCard {
+            @Override
+            public void execute(Response response) {
+                actionHandler.store_sellCard(response);
+            }
         }
-
     }
 }
